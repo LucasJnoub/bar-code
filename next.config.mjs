e4a96df67a2726224e44857567c46bc68529747a
LucasJnoub <lucas.jnoub@gmail.com>
2024-04-21 08:30:09 -0300

    trying to fix 404 on pwa

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,8 @@
 /** @type {import('next').NextConfig} */
- 
-import withPWA from 'next-pwa';
- 
-const config = {
-  cacheOnFrontEndNav: true,
-  reloadOnOnline:true,
-  swcMinify: true,
-  disable:false,
-  workboxOptions:{
-    disableDevLogs:true
-  }
-};
- 
-const nextConfig = withPWA({
-  dest: 'public',
-})(
-  config
-);
- 
-export default nextConfig;
+import { withPWA } from 'next-pwa';
+
+module.exports = withPWA({
+  pwa: {
+    dest: 'public'
+  },
+});
